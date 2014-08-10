0a34f87a128dc78de58bc63f6ec7c5823dc28662
Ben Deschamps <ben@t8pe.com>
2014-08-09 20:57:16 -0400

    Wrote basic player-functions.js file - no working code, just structure

---
+++ js/player-functions.js
@@ -0,0 +1,30 @@
+/* 
+* Player functions for the bd-jplayer plugin, to integrate it into t8pe - payments, play tracking etc.
+* v0.1
+* Ben Deschamps
+*/
+
+function t8pe1_count_play(){
+// When play reaches 30 seconds, this triggers a PHP script to increment the "plays" count by 1.
+    alert('t8pe1_count_play successfully triggered!');
+}
+
+function t8pe1_trigger_pay(){
+// When play ends, this takes $chosen_price and transfers it from User's account to Artist's account.
+    alert('t8pe1_trigger_pay successfully triggered!');
+}
+
+function t8pe1_add_to_playlist(){
+// Code available from jPlayer.playlist, perhaps? This may be unnecessary duplication.
+}
+
+function t8pe1_remove_from_playlist(){
+// Code available from jPlayer.playlist, perhaps? This may be unnecessary duplication.
+}
+
+function t8pe1_save_playlist(){
+// Triggered when hitting "Save Playlist"
+}
+
+
+
